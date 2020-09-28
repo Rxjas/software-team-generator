e3fbb7c24aee073323c1c73c670b9e4265681d90
Christopher Rojas <christopherrojas35@yahoo.com>
2020-09-28 14:14:31 -0600

    added code to initialize the program, also added Qs

--- app.js
+++ app.js
@@ -9,11 +9,171 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choices = require("inquirer/lib/objects/choices");
+
+//arrays to push the employees
+const arrayManagers = [];
+const arrayEngineers = [];
+const arrayInterns = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const start =([{
+    type: 'list',
+    name: 'start',
+    message: 'Would you like to begin the Software Team Generator?',
+    choices: ['Yes', 'No']
+}]) 
+
+const employeeRole =([{
+    type: 'list',
+    name: 'role',
+    message: 'What is the role of the Employee?',
+    choices: ['Manager', 'Engineer', 'Intern']
+}])
+
+const managerQs = ([
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of the Manager?',
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of the Manager?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the E-mail of the Manager?',
+    },
+
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is the office number of the Manager?',
+    }
+]);
+
+const engineerQs = ([
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of the Engineer?',
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of the Engineer?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the E-mail of the Engineer?',
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the GitHub username of the Engineer?',
+    }
+]);
+
+const internQs = ([
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of the Intern?',
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of the Intern?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the E-mail of the Intern?',
+    },
+
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Where did the Intern go to school?',
+    }
+]);
+
+function createTeam(data){
+    const role = data.role
+    if(role === 'Manager'){
+        inquirer.prompt(managerQs)
+            .then(function(answer){
+                const name = answer.name
+                const id = answer.id
+                const email = answer.email
+                const office = answer.office
+
+                let manager = new Manager(name, id, email, office);
+                console.log(manager)
+                // arrayManagers.push(manager);
+            })
+    } 
+    else if (role === 'Engineer'){
+        inquirer.prompt(engineerQs)
+            .then(function(answer){
+                const name = answer.name
+                const id = answer.id
+                const email = answer.email
+                const github = answer.github
+
+                let engineer = new Engineer(name, id, email, github);
+                arrayEngineers.push(engineer);
+            })
+    }
+    else if (role === 'Intern'){
+        inquirer.prompt(internQs)
+            .then(function(answer){
+                const name = answer.name
+                const id = answer.id
+                const email = answer.email
+                const school = answer.school
+
+                let intern = new Intern(name, id, email, school)
+                arrayInterns.push(intern);
+            })
+    }
+}
+
+function init(){
+    //question to initialize program
+    inquirer.prompt(start)
+        .then(function (data){
+
+            //Ask for the employee's role
+            if (data.start === 'Yes'){
+                inquirer.prompt(employeeRole)
+                    .then(function (data){
+                        createTeam(data);
+                    })
+            } 
+
+            //if the user chooses to not start the program
+            else{
+                console.log('Run Application When Ready')
+            };
+
+        })// end of start THEN
+}//end of init
+init();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
