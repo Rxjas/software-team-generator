f249606f530b6b4c93d6f7cb8ad4d6dabff86a6c
Christopher Rojas <christopherrojas35@yahoo.com>
2020-10-07 17:42:21 -0600

    Deleted unused code

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ const Choices = require("inquirer/lib/objects/choices");
 //arrays to push the employees
 const employeeArray = [];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 const start =([{
     type: 'list',
     name: 'start',
@@ -161,45 +157,6 @@ function addEmployee(){
         })
 };
 
-
-// function createTeam(data){
-//     const role = data.role
-
-//     if(role === 'Manager'){
-//         inquirer.prompt(managerQs)
-//             .then(function(answer){
-            
-//                 let manager = new Manager(name, id, email, office);
-//                 console.log(manager)
-//                 // arrayManagers.push(manager);
-//             })
-//     } 
-//     else if (role === 'Engineer'){
-//         inquirer.prompt(engineerQs)
-//             .then(function(answer){
-//                 const name = answer.name
-//                 const id = answer.id
-//                 const email = answer.email
-//                 const github = answer.github
-
-//                 let engineer = new Engineer(name, id, email, github);
-//                 arrayEngineers.push(engineer);
-//             })
-//     }
-//     else if (role === 'Intern'){
-//         inquirer.prompt(internQs)
-//             .then(function(answer){
-//                 const name = answer.name
-//                 const id = answer.id
-//                 const email = answer.email
-//                 const school = answer.school
-
-//                 let intern = new Intern(name, id, email, school)
-//                 arrayInterns.push(intern);
-//             })
-//     }
-// }
-
 function init(){
 
     //question to initialize program
@@ -210,31 +167,13 @@ function init(){
             if (data.start === 'Yes'){
                 addManager();
             } 
-
             //if the user chooses to not start the program
-            else{
+            else {
                 console.log('Run Application When Ready')
             };
 
         })// end of start THEN
 }//end of init
+
 init();
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
